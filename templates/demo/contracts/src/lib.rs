6c76a574b1a43ade7c25024905b5052e0dc9626c
Anthony Laibe <anthony@laibe.cc>
2018-08-17 13:33:21 +0100

    Eswam step1

---
+++ templates/demo/contracts/src/lib.rs
@@ -0,0 +1,17 @@
+extern crate ewasm_api;
+
+#[no_mangle]
+pub extern "C" fn main() {
+    let length = ewasm_api::calldata_size();
+
+    // charge a base fee plus a word fee for every 256-bit word
+    let base_fee = 15;
+    let word_fee = 3;
+    let total_cost = base_fee + ((length + 31) / 32) * word_fee;
+
+    ewasm_api::consume_gas(total_cost as u64);
+
+    let data = ewasm_api::calldata_copy(0, length);
+
+    ewasm_api::finish_data(&data);
+}
