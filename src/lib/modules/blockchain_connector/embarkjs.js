c8d8a6162693ccb700e6691748a30720d2d4a379
Jonathan Rainville <rainville.jonathan@gmail.com>
2018-11-30 11:16:09 -0500

    extract web3 functions

--- src/lib/modules/blockchain_connector/embarkjs.js
+++ src/lib/modules/blockchain_connector/embarkjs.js
@@ -3,10 +3,31 @@
 const __embarkWeb3 = {};
 
 __embarkWeb3.init = function (_config) {
-  // Actually set web3
   this.web3 = new Web3();
 };
 
 __embarkWeb3.getAccounts = function () {
   return this.web3.eth.getAccounts(...arguments);
 };
+
+__embarkWeb3.getNewProvider = function (providerName, ...args) {
+  return new Web3.providers[providerName](...args);
+};
+
+__embarkWeb3.setProvider = function (provider) {
+  return this.web3.setProvider(provider);
+};
+
+__embarkWeb3.getCurrentProvider = function () {
+  return this.web3.currentProvider;
+};
+
+__embarkWeb3.getDefaultAccount = function () {
+  return this.web3.eth.defaultAccount;
+};
+
+__embarkWeb3.setDefaultAccount = function (account) {
+  this.web3.eth.defaultAccount = account;
+};
+
+
